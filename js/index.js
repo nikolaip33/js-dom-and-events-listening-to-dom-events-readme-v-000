e40118d7916e4b79edb667f820ee8cc99e1c6b47
Nikolai Pawlowski <nikolaip@gmail.com>
2019-07-18 03:39:05 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
 
-const input = document.querySelector('#input');
+const input = document.querySelector('input');
 
 input.addEventListener('keydown', function(e) {
   console.log(e.which);
